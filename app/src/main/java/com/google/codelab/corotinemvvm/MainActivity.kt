83e15fcaea260de527448fc2b0fc3de80a9c46ee
ike04 <boc.0409@outlook.jp>
2022-02-19 12:07:41 +0900

    First commit

---
+++ app/src/main/java/com/google/codelab/corotinemvvm/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.google.codelab.corotinemvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.codelab.corotinemvvm.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
